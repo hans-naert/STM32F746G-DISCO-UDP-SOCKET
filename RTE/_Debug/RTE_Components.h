99b2ec5de3bf23150331702271706c8b2de5f833
Hans Naert <hans.naert@vives.be>
2024-03-28 20:00:24 +0100

    Add Socket in RTE and configure with RTE_device.h and Net_Config_ETH_0.h

--- RTE/_Debug/RTE_Components.h
+++ RTE/_Debug/RTE_Components.h
@@ -23,6 +23,10 @@
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
         #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
         #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil::CMSIS Driver:Ethernet MAC@1.12.0 */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+/* Keil::CMSIS Driver:Ethernet PHY:LAN8742A@1.3.0 */
+#define RTE_Drivers_PHY_LAN8742A        /* Driver PHY LAN8742A */
 /* Keil::Device:STM32Cube Framework:Classic@1.3.0 */
 #define RTE_DEVICE_FRAMEWORK_CLASSIC
 /* Keil::Device:STM32Cube HAL:Common@1.3.0 */
@@ -37,6 +41,20 @@
 #define RTE_DEVICE_HAL_RCC
 /* Keil::Device:Startup@1.2.5 */
 #define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+/* Keil::Network&MDK-Pro:CORE&IPv4/IPv6 Release@7.19.0 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+          #define RTE_Network_IPv6                /* Network IPv6 Stack */
+          #define RTE_Network_Release             /* Network Release Version */
+/* Keil::Network&MDK-Pro:Interface:ETH@7.19.0 */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+
+/* Keil::Network&MDK-Pro:Socket:BSD@7.19.0 */
+#define RTE_Network_Socket_BSD          /* Network Socket BSD */
+/* Keil::Network&MDK-Pro:Socket:TCP@7.19.0 */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+/* Keil::Network&MDK-Pro:Socket:UDP@7.19.0 */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
 
 
 #endif /* RTE_COMPONENTS_H */
