c1b5dea344ef7c14cd365d56dabd6aa1cd2bceda
Hans Naert <hans.naert@vives.be>
2024-03-28 19:49:58 +0100

    Copy blinky example

---
+++ RTE/_Debug/RTE_Components.h
@@ -0,0 +1,42 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/* ARM::CMSIS-View:Event Recorder&DAP@1.5.3 */
+#define RTE_CMSIS_View_EventRecorder
+        #define RTE_CMSIS_View_EventRecorder_DAP
+/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.8.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil::Device:STM32Cube Framework:Classic@1.3.0 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/* Keil::Device:STM32Cube HAL:Common@1.3.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex@1.3.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:GPIO@1.3.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:PWR@1.3.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC@1.3.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:Startup@1.2.5 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+
+
+#endif /* RTE_COMPONENTS_H */
